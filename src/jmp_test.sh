b3da1b0ba02aa84a0117012f02ca9cd19777522e
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-08-07 11:06:19 +0200

    use .Label to exclude the simbols from the resulting executable

--- src/jmp_test.sh
+++ src/jmp_test.sh
@@ -1,8 +1,9 @@
 
 
-sh analyse_instruction.sh "jmp LoooooooooooooooooooooongLabel{loop_id} S LoooooooooooooooooooooongLabel{loop_id}:" 100000 50000 
-sh analyse_instruction.sh "jmp L{loop_id} S L{loop_id}:" 100000 50000 
-sh analyse_instruction.sh "jmp L{loop_id} S L{loop_id}:" 1000 5000000 
+sh analyse_instruction.sh "jmp .LoooooooooooooooooooooongLabel{loop_id} S .LoooooooooooooooooooooongLabel{loop_id}:" 10000 50000 
+sh analyse_instruction.sh "jmp .L{loop_id} S .L{loop_id}:" 5000 500000
+sh analyse_instruction.sh "jmp .L{loop_id} S .L{loop_id}:" 2500 500000 
+sh analyse_instruction.sh "jmp .L{loop_id} S .L{loop_id}:" 1000 500000 
 
 
 
